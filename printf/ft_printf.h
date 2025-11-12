b015f60c78b34bd23c8b793bf7e25215b5e1042d
Yoru <swearwings@gmail.com>
2025-11-12 14:59:32 +0000

    added subject, finished printf

--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -6,22 +6,30 @@
 /*   By: jzorreta <jzorreta@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 23:41:08 by jzorreta          #+#    #+#             */
-/*   Updated: 2025/11/12 00:23:05 by jzorreta         ###   ########.fr       */
+/*   Updated: 2025/11/12 14:54:57 by jzorreta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+# include <stdarg.h>
 # include <unistd.h>
 
-# define HEX_HIGH "123456789ABCDEF"
-# define HEX_LOW "123456789abcdef"
+typedef unsigned int		t_ui;
+typedef unsigned long		t_ul;
+typedef unsigned long long	t_ull;
 
-int	ft_putchar(char c);
-int ft_putstr(char *str);
-int	ft_putnbr(long n);
+# define HEX_LOW "0123456789abcdef"
+# define HEX_HIGH "0123456789ABCDEF"
 
-int ft_printf(const char *, ...);
+int	ft_printf(const char *str, ...);
+int	ft_argument_checker(char c, va_list args);
+int	ft_putchar(int c);
+int	ft_putstr(char *str);
+int	ft_putnbr_hex(t_ul nb, char *BASE);
+int	ft_putptr(t_ull ptr);
+int	ft_putnbr(long nb);
+int	ft_putnbr_unsigned(long nb);
 
 #endif
--- printf/utils.c
+++ printf/utils.c
@@ -6,60 +6,41 @@
 /*   By: jzorreta <jzorreta@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 23:41:18 by jzorreta          #+#    #+#             */
-/*   Updated: 2025/11/12 00:20:13 by jzorreta         ###   ########.fr       */
+/*   Updated: 2025/11/12 14:55:19 by jzorreta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf.h"
+#include "ft_printf.h"
 
-int ft_putchar(char c)
+int	ft_putchar(int c)
 {
-	write (1, &c, 1); // plain old putchar...
-	return (1); // this is to bookkeep, will count how many chars were printed
+	write(1, &c, 1);
+	return (1);
 }
 
-int ft_putstr(char *str)
+int	ft_putstr(char *str)
 {
-	int i;
-	
+	int	i;
+
 	i = 0;
 	if (!str)
-		str = "null"; // no string :c, so null string :c
+		str = "(null)";
 	while (str[i])
 	{
-		write (1, &str[i], 1);
+		write(1, &str[i], 1);
 		i++;
 	}
-	return (i); // basically just a normal putstr with that lil caveat :3
+	return (i);
 }
 
-int	ft_putnbr(long n)
+int	ft_putptr(t_ull ptr)
 {
-	char	n;
-	int		num;
+	int	count;
 
-	num = 0;
-	if (n < 0)
-	{
-		num += ft_putchar('-'); // basically this just says "hey! i printed this, add count"
-		n = -n;
-	}
-	if (n >= 10)
-	{
-		num += ft_putnbr(n / 10); // same here
-	}
-	n = '0' + (n % 10);
-	num += ft_putchar(n); // same here...
-	return (num); // but overall a normal putnbr
+	count = 0;
+	if (ptr == 0)
+		return (ft_putstr("(nil)"));
+	count += ft_putstr("0x");
+	count += ft_putnbr_hex(ptr, HEX_LOW);
+	return (count);
 }
-
-// add the rest ig, i know theres for %p (pointer), and other shit im not remembering
-// its fucking midnight
-// wait let me make a list for tomorrow me
-// - add pointer %p
-// - add hexadecimal bs, hexa high, hexa low....i think thats supposed to be on the header or smth
-// - make some random bs for unsigned numbers so %u %x and %X can fucking print right mate 
-// - finish the header
-// - finish printf.c //i wanna die :3
-
-// "Faz um putnbr_base so para base 16 e quando fores usar a base usa um macro dependendo se e HEXA ou hexa"
